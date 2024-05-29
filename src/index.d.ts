66466a3f14a0a14ead0ed3876a9771b7f03d2c51
chenqi <1549914423@qq.com>
2024-05-29 16:06:45 +0800

    feat():初始化

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "vite";
 export interface IdefaultsProp {
-  unitToConvert: string,
-  viewportWidth: number,
-  unitPrecision: number,
-  viewportUnit: string,
-  fontViewportUnit: string,
-  minPixelValue: number,
+  unitToConvert?: string,
+  viewportWidth?: number,
+  unitPrecision?: number,
+  viewportUnit?: string,
+  fontViewportUnit?: string,
+  minPixelValue?: number,
 }
 
 export default function vitePluginStyleToVw(options?:IdefaultsProp):Plugin
