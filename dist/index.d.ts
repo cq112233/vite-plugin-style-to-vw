6f19ed3704b37d9d092603cfa0d6c9110cf7be74
chenqi <1549914423@qq.com>
2024-05-29 16:02:47 +0800

    feat():初始化

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,12 +1,12 @@
 import { Plugin } from 'vite';
 
 interface IdefaultsProp {
-  unitToConvert?: string,
-  viewportWidth?: number,
-  unitPrecision?: number,
-  viewportUnit?: string,
-  fontViewportUnit?: string,
-  minPixelValue?: number,
+  unitToConvert: string,
+  viewportWidth: number,
+  unitPrecision: number,
+  viewportUnit: string,
+  fontViewportUnit: string,
+  minPixelValue: number,
 }
 
 declare function vitePluginStyleToVw(options?:IdefaultsProp):Plugin
